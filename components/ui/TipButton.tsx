feea510819be07bae7d38db3daf2f6f9bf083c23
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-09-21 23:58:42 +0800

    Add styling to Tip Button for mobile view

--- components/ui/TipButton.tsx
+++ components/ui/TipButton.tsx
@@ -8,7 +8,7 @@ interface TipButtonProps {
 
 const TipButton: React.FC<TipButtonProps> = ({percentage,isSelected, onTipSelect}) => {
   return (
-    <div className={`button px-10 py-2 text-2xl rounded-md font-semibold
+    <div className={`button flex items-center justify-center tip-component
       ${isSelected ? "button-active" : 'hover:bg-[#016368] hover:-translate-y-1' }`} onClick={onTipSelect}>
       {percentage}%
     </div>

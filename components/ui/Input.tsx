c2b8c0b1e113eefccfa91e86520aa1e5485b487c
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-09-22 16:41:15 +0800

    Fix the width size of Input from 80% to 100%

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -10,7 +10,7 @@ const Input = ({placeholder, imgSrc, allowFloatingNum}: InputProps) => {
   const [isFocused, setIsFocused] = useState(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value;
+    const value = e.target.value;          
     
     if (allowFloatingNum) {
       const filtered = value.replace(/[^0-9.]/g, '');
@@ -32,7 +32,7 @@ const Input = ({placeholder, imgSrc, allowFloatingNum}: InputProps) => {
   };
 
   return (
-    <div className={`flex flex-row items-center bg-gray-200 w-[80%] h-12 text-xl lg:text-2xl rounded-md px-5 text-right font-semibold text-(--secondary-color) ${isFocused ? 'input-focus' : ''}`}>
+    <div className={`flex flex-row items-center bg-gray-200 w-full h-12 text-xl lg:text-2xl rounded-md px-5 text-right font-semibold text-(--secondary-color) ${isFocused ? 'input-focus' : ''}`}>
       <img src={imgSrc} alt="Dollar Sign" className='aspect-square w-4'/>
       <input className='flex-1 text-right focus:outline-0' placeholder={placeholder} onFocus={() => setIsFocused(true)} onBlur={() => setIsFocused(false)} onInput={handleInputChange} />
     </div>

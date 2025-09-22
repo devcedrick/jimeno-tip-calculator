ac565d57933536557d5390213ec3b5722be23587
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-09-23 01:45:19 +0800

    Validates input to prevent zero values
    Adds input validation to prevent zero values in the
    number of people field, enhancing user experience
    by displaying an error message when zero is entered.
    
    Also adds focus styling for error inputs and adjusts input
    component to handle validation state.

--- components/layouts/InputPanel.tsx
+++ components/layouts/InputPanel.tsx
@@ -4,15 +4,15 @@ import TipLayout from './TipLayout'
 
 const InputPanel = () => {
   return (
-    <div className='flex flex-col justify-between gap-6 max-sm:w-full w-max h-max p-2 lg:p-5 '>
+    <div className='flex flex-col justify-between gap-8 max-sm:w-full w-max h-max p-2 lg:p-5 '>
       <div>
         <p className='input-label mb-2'>Bill</p>
-        <Input placeholder="0" imgSrc="/images/icon-dollar.svg" allowFloatingNum={true} />
+        <Input placeholder="0" imgSrc="/images/icon-dollar.svg" allowFloatingNum={true} allowZero={true} />
       </div>
       <TipLayout/>
       <div>
         <p className='input-label mb-2'>Number of People</p>
-        <Input placeholder="0" imgSrc="/images/icon-person.svg" allowFloatingNum={false} />
+        <Input placeholder="0" imgSrc="/images/icon-person.svg" allowFloatingNum={false} allowZero={false} />
       </div>
     </div>
   )

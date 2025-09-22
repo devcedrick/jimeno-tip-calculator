5e6d3bbaaa4c0766ba58799404e4b886ec8b2abc
Ken Cedrick Jimeno <kencedrickjimeno@gmail.com>
2025-09-22 22:58:08 +0800

    Adds result panel component
    Introduces a new result panel component to display calculated
    tip and total amounts, enhancing the application layout and
    responsiveness across different screen sizes.

---
+++ components/layouts/ResultPanel.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const ResultPanel = () => {
+  return (
+    <div className='flex flex-col justify-between bg-(--secondary-color) h-full md:w-full lg:w-max p-8 rounded-xl gap-10 max-lg:min-w-[300px]'>
+      <div className='flex flex-col gap-5 lg:gap-10'>
+        <div className='flex items-center justify-between gap-15 lg:gap-25'>
+          <div className='w-max'>
+            <p className='text-sm font-semibold text-white text-nowrap '>Tip Amount</p>
+            <p className='text-xs font-semibold text-gray-400'>/ person</p>
+          </div>
+          <p className='text-(--primary-color) font-semibold text-4xl'>$0.00</p>
+        </div>
+        <div className='flex items-center justify-between gap-15 lg:gap-25'>
+          <div className='w-max'>
+            <p className='text-sm font-semibold text-white'>Total</p>
+            <p className='text-xs font-semibold text-gray-400'>/ person</p>
+          </div>
+          <p className='text-(--primary-color) font-semibold text-4xl'>$0.00</p>
+        </div>
+      </div>
+      <button className='font-semibold text-lg bg-(--primary-color) text-(--secondary-color) w-full py-3 rounded-md'>RESET</button>
+    </div>
+  )
+}
+
+export default ResultPanel
